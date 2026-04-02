e90209ee2799b0861f480cb94412c54d1f50523f
ArthurEzz <arthurpinheiro218@gmail.com>
2026-04-02 19:48:35 +0000

    dando início na sexta atividade

---
+++ segundo_trabalho_parcial/bin/sextaatvd.dart
@@ -0,0 +1,19 @@
+//Uma lanchonete está enfrentando dificuldades para atender certos pedidos
+//devido a restrições em algumas combinações do cardápio. As regras são as seguintes:
+//lanche Bauru não pode ser acompanhado de Guaraná.
+//Se o cliente escolher X-Frango, não poderá pedir Água.
+//Ao pedir Pizza, as únicas bebidas permitidas são Vinho ou Água.
+//Com base nessas restrições, desenvolva um programa que leia o lanche e a bebida
+// escolhidos pelo cliente e determine se o pedido pode ou não ser atendido.
+
+import 'dart:io';
+
+void main(){
+  stdout.write("\n== Sistema da Lanchonete ==\n");
+  stdout.write("-- Lanches --");
+  stdout.write("1 - Bauru\n");
+  stdout.write("2 - X-Frango\n");
+  stdout.write("3 - X-Tudo\n");
+  stdout.write("4 - X-Bagunça\n");
+  
+}
