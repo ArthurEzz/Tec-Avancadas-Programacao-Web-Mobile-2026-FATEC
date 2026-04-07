f366a6a7c1073c2701a744a089641adbd77b7123
ArthurEzz <arthurpinheiro218@gmail.com>
2026-04-07 21:28:10 +0000

    erro

--- terceiro_trabalho_parcial/bin/nonaatvd.dart
+++ terceiro_trabalho_parcial/bin/nonaatvd.dart
@@ -8,7 +8,7 @@ void main() {
   String resposta = "";
   do {
     stdout.write("Deseja continuar? (s/n)\n");
-  Sresposta = stdin.readLineSync()!;
+   resposta = stdin.readLineSync()!;
 
     if (resposta.toLowerCase() == "s") {
       stdout.write("Continuando...\n\n");
